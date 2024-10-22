07ef7b1103ee4de080eca3a6698ba939437f52ff
Mateuszzx <mateuszwojcikv@gmail.com>
2024-10-23 00:32:13 +0200

    Lab_2

--- Lab2/importData.m
+++ Lab2/importData.m
@@ -4,15 +4,18 @@
 filename = 'cwiczenia_A2_pliki/daneP.csv'; % Nazwa pliku
 data = uiimport(filename); % Import danych
 
-variables = data.Properties.VariableNames; 
+variables = data.colheaders;
 numVariables = numel(variables);
 % Tworzenie osobnych wykresów dla każdej zmiennej
+clf;
+figure;
+set(gcf, "Position", [100 100 1000 400])
 for i = 1:numVariables
-    figure; % Tworzenie nowego wykresu
-    plot(data{:,i}); % Rysowanie wykresu dla zmiennej i
-    title(variables{i}); % Dodawanie tytułu opartego o nazwę zmiennej
+    subplot(1, numVariables, i);
+    plot(data.data(:, i)); % Rysowanie wykresu dla zmiennej i
+    title(variables(i)); % Dodawanie tytułu opartego o nazwę zmiennej
     xlabel('Indeks'); % Etykieta osi x
-    ylabel(variables{i}); % Etykieta osi y
+    ylabel(variables(i)); % Etykieta osi y
 
 end
 
